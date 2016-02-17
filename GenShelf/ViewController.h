dc8ee2708c46fa883ae0c0a8c10a3ec1ccc2d409
dbsGen <z.rz2323721@gmail.com>
2016-02-17 16:28:22 +0800

    第一基础版本

---
+++ GenShelf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GenShelf
+//
+//  Created by Gen on 16/2/16.
+//  Copyright © 2016年 AirRaidClub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController <UIWebViewDelegate, NSURLConnectionDelegate> {
+}
+
+@end
+
---
+++ GenShelf/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GenShelf
+//
+//  Created by Gen on 16/2/16.
+//  Copyright © 2016年 AirRaidClub. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
